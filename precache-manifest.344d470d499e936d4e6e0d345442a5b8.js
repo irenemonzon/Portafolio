9c9ee6685d411a54c3b1d59ff411d9440b503310
irenemonzon <irene.monzonm@gmail.com>
2020-02-07 09:25:27 -0300

    Updates

---
+++ precache-manifest.344d470d499e936d4e6e0d345442a5b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2d85b93c764cb620af1b27ae3f526c",
+    "url": "/Portafolio/public/index.html/index.html"
+  },
+  {
+    "revision": "c28fe83ac6bfd682f8f5",
+    "url": "/Portafolio/public/index.html/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "59b9f11b960bf8201c5d",
+    "url": "/Portafolio/public/index.html/static/css/main.0c05f174.chunk.css"
+  },
+  {
+    "revision": "c28fe83ac6bfd682f8f5",
+    "url": "/Portafolio/public/index.html/static/js/2.91b93502.chunk.js"
+  },
+  {
+    "revision": "0547788a45f31a38660e5a3ce8c04577",
+    "url": "/Portafolio/public/index.html/static/js/2.91b93502.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59b9f11b960bf8201c5d",
+    "url": "/Portafolio/public/index.html/static/js/main.349e6d94.chunk.js"
+  },
+  {
+    "revision": "ba6640a9aed8ba56b9f2",
+    "url": "/Portafolio/public/index.html/static/js/runtime-main.f4af8e24.js"
+  },
+  {
+    "revision": "c6fb3bc72f70e99d55fd9d6bf68165f5",
+    "url": "/Portafolio/public/index.html/static/media/Papitas.c6fb3bc7.png"
+  },
+  {
+    "revision": "165e6b62256afa9be16d30bcd9930578",
+    "url": "/Portafolio/public/index.html/static/media/PokemonData.165e6b62.png"
+  },
+  {
+    "revision": "0a626490c8be4ef510387953c2ea9b65",
+    "url": "/Portafolio/public/index.html/static/media/movie.0a626490.png"
+  },
+  {
+    "revision": "77d4f033dd2f362e637579ce14af9468",
+    "url": "/Portafolio/public/index.html/static/media/profile.77d4f033.png"
+  }
+]);
